25cd38523c44ec664a55b7b1f2d2b354cfef2ce1
Jake Edwards <edwardsjake@live.com>
2021-01-17 21:58:19 -0600

    Added more commands from discord.js tutorial

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
+// fs is Node's native file system module
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-// create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+
+	if (command.args && !args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	// if you don't supply in command file, it'll default to 3
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.log(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
-// login to Discord with your apps's token
-client.login('');
+client.login(token);
